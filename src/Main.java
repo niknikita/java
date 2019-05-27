26d827d9c4d7ad73f4d8e69c6ef5cdcea4bf2dd1
Никита Чеботов <niknikita069@gmail.com>
2019-05-27 12:10:14 +0600

    Домашка с массивами и сортировкой

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double b = 9;
-        b += 1; // тоже самое что и a = a+1;
-        b -= 1; // тоже самое что и a = a-1;
-        System.out.println(b % 2); // результат = 1
+        //while, do while
+        //факториал
+        //сортировкак массива
+        System.out.println(fact(10));
 
+        int[] arr =  new int[10];
+        arr[0] = 1;
+        arr[1] = 5;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 6;
+        arr[5] = 9;
+        arr[6] = 7;
+        arr[7] = 8;
+        arr[8] = 2;
+        arr[9] = 6;
 
-        int r,t = 5;
+        bubbleSort(arr);
 
-
-
-        int i = Integer.MAX_VALUE;
-        System.out.println(i);
-        i++;
-        System.out.println(i);
-        // при выходе из диапазона допустимых значений значение переменная i сбросится на минимально допустимое значение
-
-        int j = Integer.MIN_VALUE;
-        System.out.println(j);
-        j--;
-        System.out.println(j);
-        // аналогично как в примере выше
-
-
-        int a = 10;
-        boolean res = a < 10;
-        if (res) {
-
+        for (int anInt : arr){
+            System.out.println(anInt);
         }
 
-        //почему boolean = 1 байт
-        //иначе никак, минимальный размер ячейки памяти = 1 байт
+    }
 
+    static int fact(int x) {
+        int rez = 1;
+        for (int i = 1; i <= x; i++) {
+            rez = rez * i;
+        }
+        return rez;
+    }
 
-//        {
-//            int m = 10;
-//        }
-//        System.out.println(m);
-        //не чего не получится будет ошибка.
-        //Использование переменной за пределами области видимости(за пределами фигурных скобок в которых она была инициализирована) невозможно
+    static int[] bubbleSort(int[] array){
+        int t;
 
-
+        for (int i=0; i<=9; i++){
+            for (int j=i+1; j<=9; j++){
+                if (array[i]>array[j]){
+                    t = array[i];
+                    array[i] = array[j];
+                    array[j] = t;
+                }
+            }
+        }
+        return array;
     }
 }
